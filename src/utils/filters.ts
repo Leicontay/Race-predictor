37f01326237d25624f54db48150a61a5ed4438ad
Leicontay <54875090+Leicontay@users.noreply.github.com>
2026-01-24 23:16:03 +0200

    Rename function to hollywoodbetsPuntersOnly

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,6 +1,6 @@
 import { Race } from '../types/Race';
 
-export function hollywoodbetsOnly(races: Race[]): Race[] {
+export function hollywoodbetsPuntersOnly(races: Race[]): Race[] {
   return races.filter(r =>
     r.meeting.toLowerCase().includes('hollywoodbets')
   );
